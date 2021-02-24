e2132d160ecbb3334ed60069afb180898a90e607
Mohankumar <mohan.17cs@kct.ac.in>
2021-02-24 10:54:02 +0530

    Completed Movement Feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,8 @@ public class Main extends PApplet {
 
     public static final int HEIGHT = 480;
     public static final int WIDTH = 640;
+    public static final int DIAMETER = 10;
+    public static int counter=0;
 
     public static void main(String[] args) {
         PApplet.main("Main",args);
@@ -13,5 +15,22 @@ public class Main extends PApplet {
     public void settings() {
         super.settings();
         size(WIDTH, HEIGHT);
+    }
+
+    @Override
+    public void draw() {
+        step();
+        showBalls();
+    }
+
+    private void showBalls() {
+        ellipse(counter,HEIGHT*1/5,DIAMETER, DIAMETER);
+        ellipse(2*counter,HEIGHT*2/5,DIAMETER, DIAMETER);
+        ellipse(3*counter,HEIGHT*3/5,DIAMETER, DIAMETER);
+        ellipse(4*counter,HEIGHT*4/5,DIAMETER, DIAMETER);
+    }
+
+    private void step() {
+        counter++;
     }
 }
